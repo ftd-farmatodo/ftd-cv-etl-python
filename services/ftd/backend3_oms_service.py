2fbf82880675b63c8f9c79b8327ee90674b21f98
Diego Poveda <diego.poveda@farmatodo.com>
2019-07-26 12:03:08 -0500

    [refactor] se movieron algunas clases de paquetes

--- services/ftd/backend3_oms_service.py
+++ services/ftd/backend3_oms_service.py
@@ -5,12 +5,12 @@ import requests
 logging.basicConfig(level=logging.DEBUG)
 logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-OMS_BASE_URL = 'http://localhost:8080/oms'
+BACKEND3_OMS_BASE_URL = 'http://localhost:8080/oms'
 
 
 def send_order_to_sim(request):
     logging.debug("method: send_order_to_sim() -> Request: " + str(request))
-    url = OMS_BASE_URL + '/v3/order/sim'
+    url = BACKEND3_OMS_BASE_URL + '/v3/order/sim'
     try:
         response = requests.post(url, data=json.dumps(request), headers={'content-type': 'application/json'})
         logging.debug("method: send_order_to_rms() -> Response: " + json.dumps(response.json()))
@@ -25,7 +25,7 @@ def send_order_to_sim(request):
 
 def send_order_to_rms(request):
     logging.debug("method: send_order_to_rms() -> Request: " + str(request))
-    url = OMS_BASE_URL + '/v3/order/rms'
+    url = BACKEND3_OMS_BASE_URL + '/v3/order/rms'
     try:
         response = requests.post(url, data=json.dumps(request), headers={'content-type': 'application/json'})
         logging.debug("method: sen_order_to_sim() -> Response: " + json.dumps(response.json()))
--- services/ftd/core2_oms_service.py
+++ services/ftd/core2_oms_service.py
@@ -5,14 +5,14 @@ import requests
 logging.basicConfig(level=logging.DEBUG)
 logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-OMS_BASE_URL = 'http://10.193.0.9:11103/DeliveryWS'
-# OMS_BASE_URL = 'http://10.232.8.2:11103/DeliveryWS'
-# OMS_BASE_URL = 'http://10.193.0.2:11103/DeliveryWS'
+CORE2_DELIVERY_BASE_URL = 'http://10.193.0.9:11103/DeliveryWS'
+# CORE2_DELIVERY_BASE_URL = 'http://10.232.8.2:11103/DeliveryWS'
+# CORE2_DELIVERY_BASE_URL = 'http://10.193.0.2:11103/DeliveryWS'
 
 
 def send_order_to_rms(request):
     logging.debug("method: send_order_to_rms() -> Request: " + str(request))
-    url = OMS_BASE_URL + '/v1/order/sendOrderToRMS'
+    url = CORE2_DELIVERY_BASE_URL + '/v1/order/sendOrderToRMS'
     try:
         response = requests.post(url, data=json.dumps(request), headers={'content-type': 'application/json'})
         logging.debug("method: send_order_to_rms() -> Response: " + json.dumps(response.json()))
@@ -27,7 +27,7 @@ def send_order_to_rms(request):
 
 def send_order_to_sim(request):
     logging.debug("method: send_order_to_sim() -> Request: " + str(request))
-    url = OMS_BASE_URL + '/v1/order/sendOrderToSIM'
+    url = CORE2_DELIVERY_BASE_URL + '/v1/order/sendOrderToSIM'
     try:
         response = requests.post(url, data=json.dumps(request), headers={'content-type': 'application/json'})
         logging.debug("method: sen_order_to_sim() -> Response: " + json.dumps(response.json()))
