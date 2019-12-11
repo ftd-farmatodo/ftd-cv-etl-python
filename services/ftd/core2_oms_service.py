f28995553ad772a0373b0880c8fb455c6b27ec81
Diego Poveda <diego.poveda@farmatodo.com>
2019-12-11 10:09:37 -0500

    Se agrego URL para pruebas contra QA

--- services/ftd/core2_oms_service.py
+++ services/ftd/core2_oms_service.py
@@ -9,7 +9,8 @@ logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 # CORE2_DELIVERY_BASE_URL = 'http://localhost:7001/DeliveryWS'
 # CORE2_DELIVERY_BASE_URL = 'http://10.193.0.9:11103/DeliveryWS'
 # CORE2_DELIVERY_BASE_URL = 'http://10.232.8.2:11103/DeliveryWS'
-CORE2_DELIVERY_BASE_URL = 'http://10.193.0.2:11103/DeliveryWS'
+CORE2_DELIVERY_BASE_URL = 'http://10.193.0.6:11103/DeliveryWS'
+# CORE2_DELIVERY_BASE_URL = 'http://10.193.0.2:11103/DeliveryWS'
 
 
 def send_order_to_rms(request):
