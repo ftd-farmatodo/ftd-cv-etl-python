e75d31b01dcfbfb8e95229f561cb72e16350d849
Intraron <Luis.Villamizar@farmatodo.com>
2020-04-07 16:07:15 -0500

    Inclusion de RUN

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
 import sys
 
 def run_rms():
-    from tasks import send_orders_to_rms_task
+    from tasks.rms import send_orders_to_rms_task
 
 def run_sim():
-    from tasks import send_orders_to_sim_task
+    from tasks.sim import send_orders_to_sim_task
+
+switcher = {
+
+    "run_sim": run_sim,
+    "run_rms": run_rms
+}
+
+
+def execute_task(argument):
+    # Get the function from switcher dictionary
+    func = switcher.get(argument, "nothing")
+    # Execute the function
+    return func()
+
+execute_task(sys.argv[1])
+
