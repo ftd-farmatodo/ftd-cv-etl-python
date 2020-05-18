97aa1d54bac95486a4d7c7547829962428905bdb
Diego Poveda <diego.poveda@farmatodo.com>
2020-05-18 18:11:25 -0500

    [release] se libera version para desplegar en produccion con cambio en los servicios q invoca al intentar reenviar las ordenes a RMS/SIM.

--- services/sim/sim_db_service.py
+++ services/sim/sim_db_service.py
@@ -8,7 +8,7 @@ import cx_Oracle
 os.environ["NLS_LANG"] = "SPANISH_COLOMBIA.AL32UTF8"
 os.chdir(os.path.dirname(__file__))
 
-#ORACLE_CLIENT = str('C:\Oracle\instantclient_12_2')
+# ORACLE_CLIENT = str('C:\Oracle\instantclient_12_2')
 ORACLE_CLIENT = str("E:\gcloud_repository\CronsPython\datasources\instantclient_12_2") #En el servidor de ETL
 #ORACLE_CLIENT = str("H:\Intraron\Proyectos\Py\Tareas\CronsPython\datasources\instantclient_12_2") #En mi maquina
 ORACLE_USER = "sim"
@@ -23,13 +23,13 @@ def connect():
 
 
 def get_orders_to_send_to_sim():
-    # return [[7072305]]
+    # return [[7999337]]
     query = """ select distinct cust_order_id, external_id, TRANSACTION_EXTENDED_ID, transaction_date_time
                 from pos_Transaction pt, pos_transaction_log l
                 where processing_status = 2
                   and not exists (select 1 from ful_ord fo where fo.cust_order_id = pt.cust_order_id)
                   -- and trunc(transaction_Date_time)<> trunc(sysdate) -- todas las ordenes desde el inicio
-                  and transaction_Date_time between TRUNC(SYSDATE-7) and TRUNC(SYSDATE) -- ordenes del dia anterior
+                  and transaction_Date_time between TRUNC(SYSDATE-1) and TRUNC(SYSDATE) -- ordenes del dia anterior
                   -- and transaction_Date_time between to_date('23/03/2020', 'dd/mm/yyyy') and to_date('06/04/2020' ,'dd/mm/yyyy') -- ordenes para un rango de fecha especifico
                   and pt.id = l.transaction_id
                   and l.MESSAGE like 'Invalid customer order ID or invalid customer order state for intended action.' """
@@ -38,3 +38,4 @@ def get_orders_to_send_to_sim():
     res = cursor.execute(query).fetchall()
     con.close()
     return res
+
