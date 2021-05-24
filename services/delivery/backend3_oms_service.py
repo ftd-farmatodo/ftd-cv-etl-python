4e216b350df8d895cd01d461a7219aaf4900a1b0
cristhian rodriguez <cristhian.rodriguez@farmatodo.com>
2021-05-24 17:39:12 -0500

    format.

--- services/delivery/backend3_oms_service.py
+++ services/delivery/backend3_oms_service.py
@@ -31,7 +31,17 @@ def send_order_to_rms(request):
 def send_order_to_sim(request):
     logging.debug("method: send_order_to_sim() -> Request: " + str(request))
     try:
-        url = BACKEND3_OMS_BASE_URL + '/oms/v3/order/' + str(request) + '/sim'
+        if str(request).find('-') != -1:
+
+            aux = str(request).find('-')
+            order_id = str(request)[0:aux]
+            post = str(request)[aux+1:len(str(request))]
+
+            url = BACKEND3_OMS_BASE_URL + '/oms/v3/order/' + order_id + '/sim?postfix=' + post
+        else:
+
+            url = BACKEND3_OMS_BASE_URL + '/oms/v3/order/' + str(request) + '/sim'
+
         logging.info("method: send_order_to_sim() -> URL Service: " + url)
         response = requests.post(url, params=payload, headers=headers)
         logging.debug("method: send_order_to_sim() -> Response: " + json.dumps(response.json()))
