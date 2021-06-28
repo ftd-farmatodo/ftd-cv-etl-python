f5c19e254db88c80c7f23f16f95ee91232b63df2
cristhian rodriguez <cristhian.rodriguez@farmatodo.com>
2021-06-28 12:01:56 -0500

    Pacial SIM 2

--- run.py
+++ run.py
@@ -5,14 +5,18 @@ def run_rms():
     from tasks.rms import send_orders_to_rms_task
 
 
-def run_sim():
-    from tasks.sim import send_orders_to_sim_task
+def run_sim_col():
+    from tasks.sim import send_orders_to_sim_task_col
+
+def run_sim_vzl():
+    from tasks.sim import send_orders_to_sim_task_vzl
 
 
 switcher = {
 
-    "run_sim": run_sim,
-    "run_rms": run_rms
+    "run_sim_col": run_sim_col,
+    "run_rms": run_rms,
+    "run_sim_vzl": run_sim_vzl
 }
 
 
